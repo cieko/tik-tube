c0bad958a90db724476a9adb7460c7d7141887b6
Cieko <110197023+cieko@users.noreply.github.com>
2025-05-16 17:32:18 +0530

    Studio layout preparation

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { UserCircleIcon } from 'lucide-react'
+import { Clapperboard, UserCircleIcon } from 'lucide-react'
 import { UserButton, SignInButton, SignedIn, SignedOut } from '@clerk/nextjs'
 
 import { Button } from '@/components/ui/button'
@@ -9,7 +9,16 @@ export const AuthButton = () => {
   return (
     <>
       <SignedIn>
-        <UserButton />
+        <UserButton>
+          <UserButton.MenuItems>
+            <UserButton.Link 
+              label='Studio'
+              href='/studio'
+              labelIcon={<Clapperboard className='size-4' />}
+            />
+            <UserButton.Action label='manageAccount' />
+          </UserButton.MenuItems>
+        </UserButton>
       </SignedIn>
       <SignedOut>
         <SignInButton mode='modal'>
